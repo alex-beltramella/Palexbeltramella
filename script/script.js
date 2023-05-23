579e0afed589dfd32b66dee8ddf08662f24bf904
Alex Beltramella <alexbeltra2003@gmail.com>
2023-05-23 17:13:07 -0300

    tuve que empezar de nuevo, agregue scss y estoy agregando los productos nuevos

---
+++ script/script.js
@@ -0,0 +1,152 @@
+//PRODUCTOS
+const productos = [
+    //INSTRUMENTOS DE PERCUSION
+    {
+        id: "Bateria",
+        titulo: "Bateria Mapex Prodigy",
+        imagen: "./img/inst-percusion/bateria-mapex-prodigy.webp",
+        categoria: {
+            nombre: "percusion",
+            id: "percusion"
+        },
+        precio: 10000
+
+    },
+    {
+        id: "hang",
+        titulo: "Hang",
+        imagen: "./img/inst-percusion/hang.webp",
+        categoria: {
+            nombre: "percusion",
+            id: "percusion"
+        },
+        precio: 10000
+
+    },
+    {
+        id: "tambor",
+        titulo: "Tambor Bombo Surdo",
+        imagen: "./img/inst-percusion/tambor.webp",
+        categoria: {
+            nombre: "percusion",
+            id: "percusion"
+        },
+        precio: 10000
+    },
+    {
+        id: "timbaleta",
+        titulo: "Timbaleta",
+        imagen: "./img/inst-percusion/timbal.webp",
+        categoria: {
+            nombre: "percusion",
+            id: "percusion",
+        },
+        precio: 10000
+    },
+    {
+        id: "pandereta",
+        titulo: "Pandereta",
+        imagen: "./img/inst-percusion/pandereta.webp",
+        categoria: {
+            nombre: "percusion",
+            id: "percusion",
+        },
+        precio: 10000
+    },
+
+    //INSTRUMENTOS DE CUERDA
+    {
+        id: "arpa",
+        titulo: "Arpa Walet Profesional",
+        imagen: "./img/inst-cuerda/arpa.webp",
+        categoria: {
+            nombre: "cuerda",
+            id: "cuerda",
+        },
+        precio: 10000
+    },
+    {
+        id: "cello",
+        titulo: "Cello",
+        imagen: "./img/inst-cuerda/chelo.webp",
+        categoria: {
+            nombre: "cuerda",
+            id: "cuerda",
+        },
+        precio: 10000
+    },
+    {
+        id: "contrabajo",
+        titulo: "Contrabajo",
+        imagen: "./img/inst-cuerda/contrabajo.webp",
+        categoria: {
+            nombre: "cuerda",
+            id: "cuerda",
+        },
+        precio: 10000
+    },
+    {
+        id: "guitarra-acustica",
+        titulo: "Guitarra Acustica Parquer",
+        imagen: "./img/inst-cuerda/guitarra-acustica.webp",
+        categoria: {
+            nombre: "cuerda",
+            id: "cuerda",
+        },
+        precio: 10000
+    },
+    {
+        id: "violin",
+        titulo: "Violin",
+        imagen: "./img/inst-cuerda/violin.webp",
+        categoria: {
+            nombre: "cuerda",
+            id: "cuerda",
+        },
+        precio: 10000
+    },
+
+    //INSTRUMENTOS DE VIENTO
+    {
+        id: "clarinete",
+        titulo: "Clarinete Knight",
+        imagen: "./img/inst-cuerda/clarinete.webp",
+        categoria: {
+            nombre: "viento",
+            id: "viento",
+        },
+        precio: 10000
+    },
+    {
+        id: "flauta",
+        titulo: "Flauta Dulce Yamaha",
+        imagen: "./img/inst-cuerda/flauta.webp",
+        categoria: {
+            nombre: "viento",
+            id: "viento",
+        },
+        precio: 10000
+    },
+    {
+        id: "trombon",
+        titulo: "Trombon Knight",
+        imagen: "./img/inst-cuerda/trombon.webp",
+        categoria: {
+            nombre: "viento",
+            id: "viento",
+        },
+        precio: 10000
+    },
+    {
+        id: "trompeta",
+        titulo: "Trompeta Knight",
+        imagen: "./img/inst-cuerda/trompeta.webp",
+        categoria: {
+            nombre: "viento",
+            id: "viento",
+        },
+        precio: 10000
+    },
+];
+
+
